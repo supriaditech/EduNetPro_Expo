7bb2542e237b95129afa257f57fd3d7ac8393555
Thimoss <thimotipandia4880@gmail.com>
2024-09-07 11:18:13 +0700

    change materi

--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -1,26 +1,9 @@
 // assets/images/index.js
 export const images: { [key: string]: any } = {
-  k3lh: require("../images/k3lh.jpg"),
-  componentscomputer: require("../images/components-computer.jpg"),
-  oscomputer: require("../images/os-computer.png"),
-  keyboard: require("../images/keyboard.png"),
-  mouse: require("../images/mouse.png"),
-  cpu: require("../images/cpu.jpg"),
-  mobo: require("../images/mobo.jpg"),
-  rom: require("../images/rom.jpg"),
-  ram: require("../images/ram.jpg"),
-  monitor: require("../images/monitor.jpg"),
-  printer: require("../images/printer.jpg"),
-  cpunew: require("../images/cpunew.jpg"),
-  management: require("../images/management.png"),
-  software: require("../images/software.jpg"),
-  pengelamatan: require("../images/pengelamatan.jpg"),
-  jaringan: require("../images/jaringan.png"),
-  permasalahan: require("../images/permasalahan.jpg"),
-  merawat: require("../images/merawat.jpg"),
-  perbaikanjaringan: require("../images/perbaikanjaringan.jpg"),
-  merawatjaringan: require("../images/merawatjaringan.jpg"),
-  masalahjaringan: require("../images/masalahjaringan.jpg"),
+  salutation: require("../images/salutation.jpg"),
+  perkenalan: require("../images/perkenalan.jpeg"),
+  lampau: require("../images/lampau.jpg"),
+  pronom: require("../images/pronom.jpg"),
 
   // Tambahkan gambar lainnya di sini
 };
